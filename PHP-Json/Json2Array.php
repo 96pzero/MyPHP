749a4d7cbbe36c549b3f448a448aaa4f5ae5bd11
96pzero <46878251+96pzero@users.noreply.github.com>
2019-04-11 15:41:47 +0800

    Create Json2Array.php

---
+++ PHP-Json/Json2Array.php
@@ -0,0 +1,6 @@
+<?php 
+	$json ='{"a":"1","b":"我","c":"cd","d":"一","e":"别生气了"}'; 
+	var_dump(json_decode($json));
+    echo"<br/><a href='https://blog.csdn.net/qq_37682202/article/details/79206181'></a>";   
+	var_dump(json_decode($json,true)); 
+ ?>
